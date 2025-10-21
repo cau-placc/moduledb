6c81d0b9c0e0b771f5020d9864ca1fcf0198a0b5
Michael Hanus <mh@informatik.uni-kiel.de>
2025-10-21 10:31:41 +0200

    cleanup scripts removed since they are no more necessary

--- scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Remove older temporary files in the module DB
-
-# start the corresponding web script:
-curl --max-time 30 -s https://moduldb.informatik.uni-kiel.de/cleanup.cgi > /dev/null 2>&1
-
-# insert in crontab with: crontab -e --> insert line:
-# 5,35 * * * * .../scripts/cleanup.sh
-# Effect: cleanup is called every 30 minutes
